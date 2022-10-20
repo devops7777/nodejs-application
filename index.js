0769d559ecc0d11d5436b8be014b52894e91455a
devops7777 <prasanthkumarperuri7777@gmail.com>
2022-10-20 14:04:20 +0000

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'Hello  this is prasanth\n'
+  const msg = 'prasanth kumar peruri\n'
   res.end(msg);
 });
 
