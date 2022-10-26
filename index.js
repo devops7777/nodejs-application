8704f85530a26599f5627192e9c2b03c569ddd32
devops7777 <prasanthkumarperuri7777@gmail.com>
2022-10-26 19:40:33 +0000

    hero

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'prasanth hero\n'
+  const msg = 'prasanth perui\n'
   res.end(msg);
 });
 
