65b311c1cc72e400c90887ab96aa5f8c0795c9c5
devops7777 <106435183+devops7777@users.noreply.github.com>
2022-11-03 21:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'perui\n'
+  const msg = 'perui prasanth kumar\n'
   res.end(msg);
 });
 
