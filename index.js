d500269fe5d464d1ed9bfc8c4cff154d1b873937
devops7777 <106435183+devops7777@users.noreply.github.com>
2022-12-09 16:51:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'iam jenkins slave\n'
+  const msg = 'iam job seeker\n'
   res.end(msg);
 });
 
