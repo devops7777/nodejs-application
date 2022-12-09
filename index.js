2329fb6183b6efe0fc715b6aecea66f011e117cc
devops7777 <106435183+devops7777@users.noreply.github.com>
2022-12-09 16:53:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'iam job seeker\n'
+  const msg = 'job searching\n'
   res.end(msg);
 });
 
