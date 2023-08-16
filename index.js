f2c796f570a609c00c43268d165ef574f815b90d
devops7777 <106435183+devops7777@users.noreply.github.com>
2023-08-16 18:37:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'demo\n'
+  const msg = 'Aman-devoloper\n'
   res.end(msg);
 });
 
