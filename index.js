00e0e337511ff7c49ef128311a8d65bb81b8b40f
devops7777 <106435183+devops7777@users.noreply.github.com>
2023-08-24 13:20:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = 'Greetings-Ton\n'
+  const msg = 'Hi-Ton\n'
   res.end(msg);
 });
 
